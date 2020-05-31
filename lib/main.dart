6d26bb93489187a045e63d2845eb1f2169253533
Tanner <tdyer@hawk.iit.edu>
2020-05-31 14:40:51 -0500

    added appbar, backgroundcolor, and body to scaffold with some styling. body is a centered image from the network.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,13 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           title: Text('I Am Rich'),
+          backgroundColor: Colors.grey[800],
+        ),
+        backgroundColor: Colors.grey[700],
+        body: Center(
+          child: Image( // think of this Image as the picture frame
+            image: NetworkImage('https://www.hdwallpaper.nu/wp-content/uploads/2015/02/chicago-19165.jpg'), // think of this image as the actual picture
+          ),
         ),
       ),
     ),
