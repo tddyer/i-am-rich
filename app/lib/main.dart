93b2de3c01662d688557069299fac69c9bdf67a3
Tanner <tdyer@hawk.iit.edu>
2020-06-01 16:49:03 -0500

    Added useless button to go along with the irony of the app as well as for extra practice with widgets

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,18 +2,32 @@ import 'package:flutter/material.dart';
 
 // main function is the starting point for all flutter apps
 void main() {
+
+  Color appBarColor = Colors.grey[800];
+  Color bgColor = Colors.grey[700];
+  bool isDark = true;
+
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.grey[800],
+          title: Center(child: Text('I Am Rich')),
+          backgroundColor: appBarColor,
         ),
-        backgroundColor: Colors.grey[700],
+        backgroundColor: bgColor,
         body: Center(
           child: Image( // think of this Image as the picture frame
             image: AssetImage('images/diamond.png'), // think of this image as the actual picture
           ),
+        ),
+        floatingActionButton: RawMaterialButton(
+          onPressed: null,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text('Do Nothing'),
+          ),
+          fillColor: Colors.red[400],
         ),
       ),
     ),
